8d0c939b91e910f495ae8b4e110f9688275158bd
Shantanu Tushar <shantanu@sostronk.com>
2015-02-18 00:07:16 +0530

    magic with the sensor data transmitter

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/gdamore/mangos"
-	"github.com/gdamore/mangos/protocol/push"
-	"github.com/gdamore/mangos/transport/ipc"
+	"github.com/gdamore/mangos/protocol/pair"
 	"github.com/gdamore/mangos/transport/tcp"
 	"github.com/kidoman/embd"
 	"github.com/kidoman/embd/sensor/l3gd20"
@@ -17,29 +17,37 @@ import (
 	_ "github.com/kidoman/embd/host/all"
 )
 
-const url = "tcp://127.0.0.1:40899"
+const url = "tcp://192.168.2.1:40899"
 
 func main() {
+	sock, err := pair.NewSocket()
+	if err != nil {
+		panic(err)
+	}
+
+	sock.AddTransport(tcp.NewTransport())
+	if err := sock.Dial(url); err != nil {
+		fmt.Errorf("could not dial %v", url)
+		panic(err)
+	}
+
+	sock.SetOption(mangos.OptionRecvDeadline, 5*time.Second)
+	fmt.Println("waiting for a message from app")
+	msg, err := sock.Recv()
+	if err != nil {
+		fmt.Errorf("could not receive from %v", url)
+		panic(err)
+	}
+	fmt.Println("received msg %v to start sending sensor data", msg)
+
 	if err := embd.InitI2C(); err != nil {
 		panic(err)
 	}
 	defer embd.CloseI2C()
 
-	var sock mangos.Socket
-	var err error
-	if sock, err = push.NewSocket(); err != nil {
-		panic(err)
-	}
-	sock.AddTransport(ipc.NewTransport())
-	sock.AddTransport(tcp.NewTransport())
-	if err = sock.Dial(url); err != nil {
-		panic(err)
-	}
-	defer sock.Close()
-
 	bus := embd.NewI2CBus(1)
 
-	gyro := l3gd20.New(bus, l3gd20.R250DPS)
+	gyro := l3gd20.New(bus, l3gd20.R2000DPS)
 	defer gyro.Close()
 
 	gyro.Start()
@@ -52,12 +60,18 @@ func main() {
 		panic(err)
 	}
 
+	sendUpdate := time.NewTicker(100 * time.Millisecond)
+	defer sendUpdate.Stop()
+
+	var orientation l3gd20.Orientation
+
 	for {
 		select {
-		case orientation := <-orientations:
+		case orientation = <-orientations:
+		case <-sendUpdate.C:
 			msg := fmt.Sprintf("%.f,%.f,%.f", orientation.X, orientation.Y, orientation.Z)
 			fmt.Printf("sending %q\n", msg)
-			go sock.Send([]byte(msg))
+			sock.Send([]byte(msg))
 		case <-quit:
 			return
 		}
